1f68c6397f2862a0ff37d4a71e5d71728ca055b7
Xxmmy <woshiyudamao@hotmail.com>
2016-10-25 17:25:41 +0800

    Delete nbvpn.sh

--- nbvpn.sh
+++
@@ -1,9 +0,0 @@
-142.0.42.16
-
-yum install git -y
-yum install gcc autoconf libtool automake make zlib-devel openssl-devel asciidoc xmlto pcre pcre-devel -y
-git clone https://github.com/shadowsocks/shadowsocks-libev.git
-cd shadowsocks-libev
-./configure && make
-make install
-
